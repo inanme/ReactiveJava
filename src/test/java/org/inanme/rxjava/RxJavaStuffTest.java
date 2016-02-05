badbe36d606c418f00c33f0b22b08e3ba3e2ba8f
Mert Inan <Mert.Inan@thomsonreuters.com>
2016-02-05 14:23:32 +0000

    subs obs

--- src/test/java/org/inanme/rxjava/RxJavaStuffTest.java
+++ src/test/java/org/inanme/rxjava/RxJavaStuffTest.java
@@ -3,6 +3,7 @@ package org.inanme.rxjava;
 import org.junit.Test;
 import rx.Observable;
 import rx.Subscriber;
+import rx.schedulers.Schedulers;
 
 import java.util.*;
 import java.util.concurrent.*;
@@ -14,9 +15,79 @@ import static org.junit.Assert.*;
 
 public class RxJavaStuffTest {
 
-    private ExecutorService service = Executors.newFixedThreadPool(2);
+    private ExecutorService service = Executors.newFixedThreadPool(3);
+
+    private ExecutorService thread1 = Executors.newCachedThreadPool(r -> new Thread(r, "thread1"));
+
+    private ExecutorService thread2 = Executors.newCachedThreadPool(r -> new Thread(r, "thread2"));
+
+    private ExecutorService thread3 = Executors.newCachedThreadPool(r -> new Thread(r, "thread3"));
 
     private Random random = new Random();
+
+    class Waiter implements Callable<Long> {
+
+        private final long l;
+
+        public Waiter(long l) {
+            this.l = l;
+        }
+
+        @Override
+        public Long call() {
+            try {
+                TimeUnit.SECONDS.sleep(l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return l;
+        }
+    }
+
+    @Test
+    public void test00() {
+        System.out.println(new Date());
+
+        Observable<Long> task1 = Observable.from(service.submit(new Waiter(2)));
+        Observable<Long> task2 = Observable.from(service.submit(new Waiter(3)));
+        Observable<Long> task3 = Observable.from(service.submit(new Waiter(4)));
+        Observable<Long> zip = Observable.zip(task1, task2, task3, (x, y, z) -> x + y + z);
+        zip.subscribe(System.out::println);
+
+        System.out.println(new Date());
+    }
+
+    @Test
+    public void test0() throws InterruptedException {
+        Observable<Integer> observable = Observable.create(subscriber -> {
+            PrimitiveIterator.OfInt iterator = IntStream.range(0, 5).iterator();
+            System.out.println(Thread.currentThread().getName() + " subs");
+            if (!subscriber.isUnsubscribed()) {
+                while (iterator.hasNext()) {
+                    subscriber.onNext(iterator.next());
+                }
+            }
+            if (!subscriber.isUnsubscribed()) {
+                subscriber.onCompleted();
+            }
+        });
+
+        observable.subscribeOn(Schedulers.from(thread1)) //
+            .observeOn(Schedulers.from(thread2)) //
+            .map(x -> {
+                //System.out.println(Thread.currentThread().getName() + " map");
+                return x + 1;
+            }).subscribe(s -> System.out.println(Thread.currentThread().getName() + " observe" + s));
+
+        observable.subscribeOn(Schedulers.from(thread1)) //
+            .observeOn(Schedulers.from(thread3)) //
+            .map(x -> {
+                //System.out.println(Thread.currentThread().getName() + " map");
+                return x * 2;
+            }).subscribe(s -> System.out.println(Thread.currentThread().getName() + " observe" + s));
+
+        TimeUnit.SECONDS.sleep(3l);
+    }
 
     @Test
     public void test1() {
